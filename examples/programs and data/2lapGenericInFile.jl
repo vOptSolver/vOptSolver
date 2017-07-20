f6c2a962e44a549363b3120d616456c70b98a2c4
vOpt <vopt@univ-nantes.fr>
2017-07-20 15:38:14 -0400

    Update 2lapGenericInFile.jl

--- examples/programs and data/2lapGenericInFile.jl
+++ examples/programs and data/2lapGenericInFile.jl
@@ -1,4 +1,6 @@
-       
+# Example of vOptGeneric, for a 2IP (the 2ap03.mop is a 2LAP), when the numerical instance is read on a file, according to the MOP format
+# July 2017
+
 using vOptGeneric
 using GLPK ; using GLPKMathProgInterface
 
