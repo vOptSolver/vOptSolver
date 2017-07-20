06d335f7c7fd32ed36be616a5ef90dcbea0069f4
vOpt <vopt@univ-nantes.fr>
2017-07-20 15:31:59 -0400

    Update 2lapSpecific.jl

--- examples/programs and data/2lapSpecific.jl
+++ examples/programs and data/2lapSpecific.jl
@@ -1,18 +1,18 @@
+# Example of vOptSpecific, for the 2LAP
+# July 2017
+
 n  =  3
 
-C1 = [  3   9   7       ;
+C1 = [  3    9   7     ;
        16   10   6     ;
-        2   7  11        ]
+        2    7  11       ]
 
 C2 = [ 16   15   6     ;
-       5   7  13     ;
-        1   2  13          ]
+        5    7  13     ;
+        1    2  13       ]
 
 using vOptSpecific   
        
 m = set2LAP( n , C1 , C2 )
 
 z1, z2, S = vSolve( m ) 
-
-
-
