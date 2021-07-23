955a093cedf56d09cdd18163bb9008923e25482c
vOpt <vopt@univ-nantes.fr>
2021-07-23 21:35:06 +0200

    Delete 2lapSpecificInFile.jl

--- examples/programs and data/2lapSpecificInFile.jl
+++
@@ -1,7 +0,0 @@
-# Example of vOptSpecific, for the 2LAP
-# when the numerical instance is read on a file, according to the LAP format
-# July 2017
-
-using vOptSpecific   
-m = load2LAP("2ap03.dat") # comment: set up the path to the file if required 
-z1, z2, S = vSolve( m ) 
